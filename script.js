c1e2b52fa6e185984ba89835295f50323085a95f
Derneira <125277800+Derneira@users.noreply.github.com>
2023-11-16 18:07:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+   function validarFormulario() {
+     const email = document.getElementById('email').value;
+     const senha = document.getElementById('password').value;
 
+     if (email === '' || senha === '') {
+       alert('Insira um email/senha válido');
+       return false;
+     }
+
+     return true;
+   }
+
+   document.getElementById('login').addEventListener('submit', function(event) {
+     event.preventDefault();
+
+     if (validarFormulario()) {
+       document.getElementById('login').style.display = 'none';
+
+       document.getElementById('logout').style.display = 'block';
+     }
+   });
+
+   document.getElementById('logoutbtn').addEventListener('click', function(event) {
+     event.preventDefault();
+
+     document.getElementById('logout').style.display = 'none';
+
+     document.getElementById('login').style.display = 'block';
+   });
